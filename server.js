84ff3b412b1f80f1a0aa21574206f3eabc8e8ed8
Maria Hirota <mhirota@dhcp-65-165.sin.redhat.com>
2018-02-06 17:04:29 +0800

    change top page and connect to mLab

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     bodyParser= require('body-parser')
-    
+
+const MongoClient = require('mongodb').MongoClient
+var db
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
@@ -13,10 +16,34 @@ app.use(bodyParser.urlencoded({extended: true}))
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
+//Accessing MongoDB on mLab
+MongoClient.connect('mongodb://babyPero:testtest@ds217898.mlab.com:17898/test-project', (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('test-project') // database name
+}}
 
+// top page - retrieve and render quotes
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html')
+    db.collection('quotes').find().toArray(function(err, result) {
+	if (err) return console.log(err)
+	console.log('-------')
+	console.log(result)
+	res.render('index.ejs', {quotes: result})
+	for(var i=0; i<result.length; i++) {
+	    console.log(result[i].name)
+	    console.log(result[i].quote)
+	}
+    })
 });
+
+//post new quotes
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+	if (err) return console.log(err)
+	console.log('saved to database')
+	res.redirect('/')
+    })
+})
 
 // error handling
 app.use(function(err, req, res, next){
