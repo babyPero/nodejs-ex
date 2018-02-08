74d644bf0018a76ae9b228c587c38833034139df
Maria Hirota <mhirota@Marias-MacBook-Air-2.local>
2018-02-08 15:09:04 +0800

    add weather api call

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var express = require('express'),
     morgan  = require('morgan');
     bodyParser= require('body-parser')
 
+const request = require('request');
 const MongoClient = require('mongodb').MongoClient
 var db
 
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs'); // set the view engine to ejs
 app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -27,13 +29,50 @@ app.get('/', function (req, res) {
     db.collection('quotes').find().sort({"_id":-1}).toArray(function(err, result) {
 	if (err) return console.log(err)
 	console.log('-------')
-	console.log(result)
 	res.render('index.ejs', {quotes: result})
 	for(var i=0; i<result.length; i++) {
 	    console.log(result[i].name)
 	    console.log(result[i].quote)
 	}
     })
+
+    /*
+    request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+	if (err) { return console.log(err)};
+	console.log(body.url);
+	console.log(body.explanation);
+	console.log("--------------------");
+    });
+
+    request('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', {json: true}, function (error, response, body) {
+	if (error) return console.log( error); // Print the error if one occurred
+	//console.log('weather forecast------------');
+	//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	//console.log(body);
+    });
+
+    request('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', {json: true}, function (error, response, body) {
+        if (error) return console.log(error); // Print the error if one occurred
+	//console.log('weather forecast------------');
+	//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //console.log(body);
+    });
+    */
+});
+
+//weather
+app.get('/weather', function (req, res) {
+    request('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', function (error, response, body) {
+	if (error) return console.log( error); // Print the error if one occurred
+	console.log('weather forecast response---------------------');
+	//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	//console.log('body---------------------');
+	console.log('body type---------------------');
+	console.log(typeof data);
+	console.log(body);
+	//console.log(data);
+        res.render('weather.ejs', {body});
+    });
 });
 
 //post new quotes
